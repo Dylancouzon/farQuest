14857104bb7c95414131d0307b61aedd220597aa
Cheng Tang <cheng21tang@yahoo.com>
2021-05-04 01:08:27 -0700

    last merge into main since I didn't save before I protected the main from administrator

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Character = require('./Character');
+const Item = require('./Item');
 
 
 
-module.exports = { User, Character };
+module.exports = { User, Character, Item };
