1a8e3e3a309818ba3d6854644981eb0cc47c8f07
Dylancouzon <dylancouzon@gmail.com>
2021-05-04 21:27:45 -0700

    Add paths

--- public/js/paths.js
+++ public/js/paths.js
@@ -4,41 +4,41 @@
 
 module.exports = {
     path0() {
-        $("#message").html(`Welcome adventurer !<br>`);
+        $("#game_message").html(`Welcome adventurer !<br>`);
 
-        $("#button1").html(`Action A`);
-        $("#button2").html(`Action B`);
-        $("#button3").html(`Action C`);
+        $("#game_button1").html(`Action A`);
+        $("#game_button2").html(`Action B`);
+        //Third button will take you to the Jinn
+        $("#game_button3").html(`Action C`);
 
 
         $("#pathId").val('1');
     },
-
+    //This is the first Path. The third route will take you to
     path1(button) {
         switch (button) {
             case 1:
-                $("#message").html(`This is path 1 <br> You selected Action A`);
-                $("#button1").html(`Action A`);
-                $("#button2").html(`Action B`);
-                $("#button3").html(`Action C`);
+                $("#game_message").html(`This is path 1 <br> You selected Action A`);
+                $("#game_button1").html(`Action A`);
+                $("#game_button2").html(`Action B`);
+                $("#game_button3").html(`Action C`);
 
-
-                $("#pathId").val('2');
+                $("#pathId").val('3');
                 break;
             case 2:
-                $("#message").html(`This is path 1 <br> You selected Action B`);
-                $("#button1").html(`Action A`);
-                $("#button2").html(`Action B`);
-                $("#button3").html(`Action C`);
+                $("#game_message").html(`This is path 1 <br> You selected Action B`);
+                $("#game_button1").html(`Action A`);
+                $("#game_button2").html(`Action B`);
+                $("#game_button3").html(`Action C`);
 
 
-                $("#pathId").val('2');
+                $("#pathId").val('3');
                 break;
             case 3:
-                $("#message").html(`This is path 1 <br> You selected Action C`);
-                $("#button1").html(`Action A`);
-                $("#button2").html(`Action B`);
-                $("#button3").html(`Action C`);
+                $("#game_message").html(`You found a Jinn in a bottle, it will grant you one wish ! <br> What do you wish for ?`);
+                $("#game_button1").html(`My Biggest dream`);
+                $("#game_button2").html(`Nothing`);
+                $("#game_button3").hide();
 
 
                 $("#pathId").val('2');
@@ -49,16 +49,61 @@ module.exports = {
     },
 
     path2(button) {
-        $("#message").html(`You have reached path 2!`);
-        $("#pathId").val('3');
+        switch (button) {
+            case 1:
+                $("#game_message").html(`$("#game_button3").hide();`);
+                $("#game_button1").html(`Action A`);
+                $("#game_button2").html(`Action B`);
+
+
+                $("#pathId").val('2');
+                break;
+            case 2:
+                $("#game_button3").show();
+                path3(1);
+
+                break;
+            default:
+                pathCheater();
+        };
     },
 
     path3(button) {
+        switch (button) {
+            case 1:
+                $("#game_message").html(`This is path 3 <br> You selected Action A`);
+                $("#game_button1").html(`Action A`);
+                $("#game_button2").html(`Action B`);
+                $("#game_button3").html(`Action C`);
 
+
+                $("#pathId").val('4');
+                break;
+            case 2:
+                $("#game_message").html(`This is path 3 <br> You selected Action B`);
+                $("#game_button1").html(`Action A`);
+                $("#game_button2").html(`Action B`);
+                $("#game_button3").html(`Action C`);
+
+
+                $("#pathId").val('4');
+                break;
+            case 3:
+                $("#game_message").html(`This is path 3 <br> You selected Action C`);
+                $("#game_button1").html(`Action A`);
+                $("#game_button2").html(`Action B`);
+                $("#game_button3").html(`Action C`);
+
+
+                $("#pathId").val('4');
+                break;
+            default:
+                pathCheater();
+        };
     },
 
     path4(button) {
-
+        $("#game_message").html(`This is path 4`);
     },
 
     path5(button) {
@@ -123,5 +168,8 @@ module.exports = {
 
     path20(button) {
 
+    },
+    pathCheater(){
+        $("#game_message").html(`How did you get here ?<br> Anyway, doesn't matter.<br> You Died !`);
     }
 }
