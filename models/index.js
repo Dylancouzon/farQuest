e99359a0066608edd8a380d4ccaa6788451c7781
Cheng Tang <cheng21tang@yahoo.com>
2021-05-04 18:46:04 -0700

    fixed FK association

--- models/index.js
+++ models/index.js
@@ -1,20 +1,10 @@
 const User = require('./User');
 const Character = require('./Character');
 const Item = require('./Item');
-// const Inventory = require('./Inventory');
 const Class = require('./Class');
 
-// Who belongs to who???
 User.hasMany(Character, {
-    // foreignKey: 'user_id'
+    foreignKey: 'id'
 });
-
-// Character.belongsTo(User, {
-//     foreignKey: 'id'
-// });
-
-// Inventory is no longer needed
-// Class has no relationship and will only be called upon to gather default stats when creating a Character
-// Item has no relationship and will only be called upon to render an object
 
 module.exports = { User, Character, Item, Class };
