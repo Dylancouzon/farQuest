b8fd6bc0a2c139e4682e1b47c02b30501f0ce856
Dylancouzon <dylancouzon@gmail.com>
2021-05-04 20:33:06 -0700

    Fix create character route

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -10,26 +10,27 @@ const auth = require(path.join(appDir, 'public', 'js', 'auth')).authTest;
 
 
 // Create character Route
-router.post('/create', async (req, res) => {
+router.post('/create', auth, async (req, res) => {
     try {
         const findClass = await Class.findByPk(req.body.character_class);
-        if (findClass) {
+        if (!findClass) {
             res.status(400).json({ message: 'Class not found !' });
             return;
         }
         const classData = findClass.get({ plain: true });
-
         const characterData = await Character.create({
             name: req.body.character_name,
             class_id: req.body.character_class,
             strength: classData.strength,
-            health: classData.health,
+            stamina: classData.stamina,
             power: classData.stamina,
             speed: classData.speed,
+            luck: classData.luck,
             attack_1: classData.attack_1,
             attack_2: classData.attack_2,
+            user_id: req.session.user_id,
         });
-
+        console.log("erreur 2");
             res.status(200).json(characterData);
 
     } catch (err) {
